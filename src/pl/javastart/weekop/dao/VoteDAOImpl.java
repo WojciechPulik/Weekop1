255e18401d0f9d5b0e85fd6766a140b8de7319aa
WojciechPulik <59091416+WojciechPulik@users.noreply.github.com>
2019-12-30 10:38:17 +0100

    adding new records, voting, jsp hidding
    repair new records adding
    voting implementation
    jsp hidding implementation

--- src/pl/javastart/weekop/dao/VoteDAOImpl.java
+++ src/pl/javastart/weekop/dao/VoteDAOImpl.java
@@ -1,23 +1,77 @@
 package pl.javastart.weekop.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 import pl.javastart.weekop.model.Vote;
+import pl.javastart.weekop.model.VoteType;
+import pl.javastart.weekop.util.ConnectionProvider;
 
 public class VoteDAOImpl implements VoteDAO{
 	
+	private static final String CREATE_VOTE = "INSERT INTO vote(discovery_id, user_id, date, type) "
+			+ "VALUES (:discovery_id, :user_id, :date, :type);";
+	private static final String READ_VOTE = "SELECT vote_id, discovery_id, user_id, date, type"
+			+ "FROM vote WHERE vote_id = :vote_id;";
+	private static final String READ_VOTE_BY_DISCOVERY_USE_ID = "SELECT vote_id, discovery_id, user_id, date, type "
+			+ "FROM vote WHERE user_id = :user_id AND discovery_id = :discovery_id;";
+	private static final String UPDATE_VOTE = "UPDATE vote SET date=:date, type=:type WHERE vote_id=:vote_id;";
+	
+	private NamedParameterJdbcTemplate template;
+	
+	public VoteDAOImpl() {
+		template = new NamedParameterJdbcTemplate(ConnectionProvider.getDataSource());
+	}
+	
+	
 	@Override
-	public Vote create(Vote newObject) {
-		return null;
+	public Vote create(Vote vote) {
+		Vote voteCopy = new Vote(vote);
+		Map<String,Object> paramMap = new HashMap<>();
+		paramMap.put("discovery_id", voteCopy.getDiscoveryid());
+		paramMap.put("user_id", voteCopy.getUserid());
+		paramMap.put("date", voteCopy.getDate());
+		paramMap.put("type", voteCopy.getVoteType().toString());
+		KeyHolder holder = new GeneratedKeyHolder();
+		SqlParameterSource paramSource = new MapSqlParameterSource(paramMap);
+		int update = template.update(CREATE_VOTE, paramSource, holder);
+		if(update > 0) {
+			voteCopy.setId(holder.getKey().longValue());
+		}
+		return voteCopy;
 	}
 	
 	@Override
 	public Vote read(Long primaryKey) {
-		return null;
+		SqlParameterSource paramSource = new MapSqlParameterSource("vote_id", primaryKey);
+		Vote vote = template.queryForObject(READ_VOTE, paramSource, new VoteRowMapper());
+		return vote;
 	}
 	
 	@Override
-	public boolean update(Vote updateObject) {
-		return false;
+	public boolean update(Vote vote) {
+		boolean result = false;
+		Map<String,Object> paramMap = new HashMap<>();
+		paramMap.put("date", vote.getDate());
+		paramMap.put("type", vote.getVoteType().toString());
+		paramMap.put("vote_id", vote.getId());
+		SqlParameterSource paramSource = new MapSqlParameterSource(paramMap);
+		int update = template.update(UPDATE_VOTE, paramSource);
+		if(update > 0) {
+			result = true;
+		}
+		return result;
 	}
 	
 	@Override
@@ -30,8 +84,31 @@ public class VoteDAOImpl implements VoteDAO{
 		return null;
 	}
 	
-	public Vote getVoteByUserIdDiscoveryId(long userid, long discoveryid) {
-		return null;
+	public Vote getVoteByUserIdDiscoveryId(long userId, long discoveryId) {
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("user_id", userId);
+		paramMap.put("discovery_id", discoveryId);
+		SqlParameterSource paramSource = new MapSqlParameterSource(paramMap);
+		Vote vote = null;
+		try {
+			vote = template.queryForObject(READ_VOTE_BY_DISCOVERY_USE_ID, paramSource, new VoteRowMapper());
+		}catch(EmptyResultDataAccessException e) {
+			//Vote not found
+		}
+		return vote;
+	}
+	
+	private class VoteRowMapper implements RowMapper<Vote>{
+		@Override
+		public Vote mapRow (ResultSet resultSet, int row) throws SQLException{
+			Vote vote = new Vote();
+			vote.setId(resultSet.getLong("vote_id"));
+			vote.setUserid(resultSet.getLong("user_id"));
+			vote.setDiscoveryid(resultSet.getLong("discovery_id"));
+			vote.setDate(resultSet.getTimestamp("date"));
+			vote.setVoteType(VoteType.valueOf(resultSet.getString("type")));
+			return vote;
+		}
 	}
 
 }
